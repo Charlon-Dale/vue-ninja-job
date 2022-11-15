93714590aeec8c7ca453e90442bcef4f9ffea1b4
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-15 11:53:30 +0800

    created router-links for jobs

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link :to="{ name: 'About' }">About</router-link>
+    <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </nav>
   <router-view/>
 </template>
