78ec183f213ac9751c38594b0444c94e97e06300
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-18 11:59:06 +0800

    added jobs.length

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,10 +1,13 @@
 <template>
     <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id" class="job">
-        <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
-            <h2>{{ job.title }}</h2>
-        </router-link>
+    <div v-if="jobs.length">
+        <div v-for="job in jobs" :key="job.id" class="job">
+            <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
+                <h2>{{ job.title }}</h2>
+            </router-link>
+        </div>
     </div>
+    
 </template>
 
 <script>
