913af375106be7a06e316ed816353ae8e62703fd
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-18 11:54:09 +0800

    added v-if and v-else

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,7 +1,13 @@
 <template>
-    <h1>{{ job.title }}</h1>
-    <p>The job id is {{ id }}</p>
-    <p>{{ job.details }}</p>
+    <div v-if="job">
+        <h1>{{ job.title }}</h1>
+        <p>The job id is {{ id }}</p>
+        <p>{{ job.details }}</p>
+    </div>
+    <div v-else>
+        <p>Loading job details...</p>
+    </div>
+
 </template>
 
 <script>
@@ -15,7 +21,7 @@ export default {
     mounted() {
         fetch('http://localhost:3000/jobs/' + this.id)
         .then(res => res.json())
-        .then(data => this.jovs = data)
+        .then(data => this.jobs = data)
         .catch(err => console.log(err.message))
     }
 }
