73805c0ef3ecd8ebd0da666b9e191ac0a39b4b38
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-18 11:28:20 +0800

    display data in db.json

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -16,6 +16,9 @@ export default {
     },
     mounted() {
         fetch('http://localhost:3000/jobs')
+        .then(res => res.json())
+        .then(data => this.jobs = data)
+        .catch(err => console.log(err.message))
     }
 }
 </script>
