46ab3cf17bad431437eaeae23096562e98d6e68a
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-18 11:22:31 +0800

    fetch localhost:3000/jobs

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -11,12 +11,11 @@
 export default {
     data() {
         return {
-            jobs: [
-                { title: 'Ninja UX Designer', id: 1, details: 'lorem' },
-                { title: 'Ninja Web Developer', id: 2, details: 'lorem' },
-                { title: 'Ninja Vue Developer', id: 3, details: 'lorem' },
-            ]
+            jobs: []
         }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs')
     }
 }
 </script>
